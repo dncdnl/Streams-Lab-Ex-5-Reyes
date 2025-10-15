2a5d364d716ff782477abdf8e81f3f9e379bcce0
dncdnl <danicadaniela.cesicar@gmail.com>
2025-10-15 18:36:31 +0800

    Created a UI for FrmLab1

--- Streams-Lab-Ex-5-Reyes/Form1.Designer.cs
+++ Streams-Lab-Ex-5-Reyes/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Streams_Lab_Ex_5_Reyes
 {
-    partial class Form1
+    partial class FrmLab1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLab1));
+            this.txtinput = new System.Windows.Forms.TextBox();
+            this.lblCreateFile = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtinput
+            // 
+            this.txtinput.BackColor = System.Drawing.Color.MistyRose;
+            this.txtinput.Location = new System.Drawing.Point(53, 78);
+            this.txtinput.Multiline = true;
+            this.txtinput.Name = "txtinput";
+            this.txtinput.Size = new System.Drawing.Size(432, 99);
+            this.txtinput.TabIndex = 0;
+            // 
+            // lblCreateFile
+            // 
+            this.lblCreateFile.AutoSize = true;
+            this.lblCreateFile.BackColor = System.Drawing.Color.Transparent;
+            this.lblCreateFile.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCreateFile.ForeColor = System.Drawing.Color.White;
+            this.lblCreateFile.Location = new System.Drawing.Point(55, 37);
+            this.lblCreateFile.Name = "lblCreateFile";
+            this.lblCreateFile.Size = new System.Drawing.Size(193, 38);
+            this.lblCreateFile.TabIndex = 1;
+            this.lblCreateFile.Text = "Create A file";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.BackColor = System.Drawing.Color.MistyRose;
+            this.btnCreate.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.ForeColor = System.Drawing.Color.Tomato;
+            this.btnCreate.Location = new System.Drawing.Point(195, 212);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(140, 40);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            // 
+            // FrmLab1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(527, 314);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblCreateFile);
+            this.Controls.Add(this.txtinput);
+            this.Name = "FrmLab1";
+            this.Text = "FrmLab1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtinput;
+        private System.Windows.Forms.Label lblCreateFile;
+        private System.Windows.Forms.Button btnCreate;
     }
 }
 
