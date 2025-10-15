58681a989ca3d9c4cd525b70c67b542f0047a514
dncdnl <danicadaniela.cesicar@gmail.com>
2025-10-15 19:26:15 +0800

    Created a public static string method above the constructor for Set File name

--- Streams-Lab-Ex-5-Reyes/FrmFileName.cs
+++ Streams-Lab-Ex-5-Reyes/FrmFileName.cs
@@ -12,16 +12,20 @@ namespace Streams_Lab_Ex_5_Reyes
 {
     public partial class FrmFileName : Form
     {
+        public static string FileName; // Static variable to hold the file name
         public FrmFileName()
         {
             InitializeComponent();
         }
-
+        public static string SetFileName()
+        {
+            return FileName; // Return the static variable value
+        }
         private void btnOkay_Click(object sender, EventArgs e)
         {
-
+          
             // Calling a static variable to get the value of txtFileName
-            String SetFileName = txtFileName.Text + "txt";
+            FileName = txtFileName.Text + "txt";
             //calling the close method
             Close();
 
